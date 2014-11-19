fa2a5fbcabb8429732405d0c4787228e20ee39cc
julian <julian.gallegos@gmail.com>
2014-11-18 21:22:17 -0800

    first commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,24 @@
+class TodosController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    @todo.save
+    redirect_to @todo
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+
+  private
+    def todo_params
+      params.require(:todo).permit(:title, :text)
+    end
+
+
+end
